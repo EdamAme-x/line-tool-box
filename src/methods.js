ce6f65e3ef9b6980367692cf3323f3b411066de5
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-20 17:05:22 +0900

    add

--- src/methods.js
+++ src/methods.js
@@ -18,7 +18,7 @@ export default function Methods() {
         //         withLoginOnExternalBrowser: true // 外部ブラウザ 
         //     }
         // ).then(() => {
-        //    alert("LIFF | Success");   
+        //    alert("success")
         // })
     }, [])
 
