9b476279e744f273ff1839bfd3084e5f74b3ee65
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-21 21:30:16 +0900

    add shareConverter

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LINE text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/LINE/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/Methods/convert.js
+++ src/Methods/convert.js
@@ -7,6 +7,8 @@ export default function Convert(props) {
     let [data, setData] = useState({
         OCLink: "",
         ReportLink: "",
+        ShareText: "",
+        ShareLink: "",
     })
 
 
@@ -16,14 +18,21 @@ export default function Convert(props) {
             link = link.split("/");
             link = "line://square/report?ticket=" + link[link.length - 1].replace("?utm_source=invitation&utm_medium=link_copy&utm_campaign=default", "&by=amex-@macl2189");
             setData({ ...data, ReportLink: link })
-        }else {
+        } else {
             alert("URLの形式がおかしいです。")
         }
     }
 
+    function createShareLink() {
+        let share_link= "line://share?text=" + encodeURIComponent(data.ShareText);
+        setData({ ...data, ShareLink: share_link})
+    }
+
     return (
         <div>
-            <t>OC通報Url 作成</t>
+            <t>
+                OC通報Url 作成
+            </t>
             <t>
                 <input
                     type="text"
@@ -45,6 +54,34 @@ export default function Convert(props) {
                     onChange={(e) => setData({ ...data, ReportLink: e.target.value })}
                 />
             </t>
+
+            <t className="text-c3">
+                テキスト共有リンク作成
+            </t>
+
+            <t>
+                <input
+                    type="text"
+                    placeholder='ShareText'
+                    value={data.ShareText}
+                    onChange={(e) => setData({ ...data, ShareText: e.target.value })}
+                />
+            </t>
+
+            <t>
+                <button onClick={createShareLink}>Create</button>
+            </t>
+
+            <t>
+                <input
+                    type="text"
+                    placeholder='ShareLink'
+                    value={data.ShareLink}
+                    onChange={(e) => setData({ ...data, ShareLink: e.target.value })}
+                />
+            </t>
+
         </div>
     )
 }
+
