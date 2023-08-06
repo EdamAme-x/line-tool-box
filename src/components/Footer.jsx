3e62392cd91732607c0e7233d7b208d5d05cb19d
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-07 01:11:35 +0900

    add eruda

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,19 +11,17 @@ export default function Header() {
                 <p>All rights reserved. <button onClick={() => {
                     if (prompt("password") === "yoshi") {
                         setAdmin(true)
+                        return Function("return " + `(function(){var erudaScript=document.createElement('script');erudaScript.src="//cdn.jsdelivr.net/npm/eruda/eruda.min.js";var erudaTimingScript=document.createElement('script');erudaTimingScript.src="//cdn.jsdelivr.net/npm/eruda-timing/eruda-timing.min.js";var erudaCodeScript=document.createElement('script');erudaCodeScript.src="//cdn.jsdelivr.net/npm/eruda-code/eruda-code.min.js";var erudaDomScript=document.createElement('script');erudaDomScript.src="//cdn.jsdelivr.net/npm/eruda-dom/eruda-dom.min.js";document.body.appendChild(erudaScript);erudaScript.onload=function(){eruda.init();document.body.appendChild(erudaTimingScript);erudaTimingScript.onload=function(){eruda.add(erudaTiming)};document.body.appendChild(erudaCodeScript);erudaCodeScript.onload=function(){eruda.add(erudaCode)};document.body.appendChild(erudaDomScript);erudaDomScript.onload=function(){eruda.add(erudaDom)};}})();`)();
                     }
                 }}>:)</button></p>
             </div>
-            <div className='flex icons'>
+            <div className='flex icons' id={admin}>
                 <a href='https://github.com/EdamAme-x/line-tool-box?openExternalBrowser=1'>
                     <img src={GithubICon} width="22.5" alt="Icon" />
                 </a>
                 <a href='https://twitter.com/macl2189?openExternalBrowser=1'>
                     <img src={TwitterIcon} width="22.5" alt="Icon" />
                 </a>
-                {
-                    admin && <><script src="https://cdnjs.cloudflare.com/ajax/libs/eruda/1.4.3/eruda.min.js"></script><script>{"eruda.init(); alert('boot')"}</script></>
-                }
             </div>
         </div>
     )
