b966a7681ced294e61be4c624dc3fd8357017606
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-08 17:15:11 +0900

    'build'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,13 @@ export default function Header() {
                         setAdmin(true)
                         return Function("return " + `(function(){var erudaScript=document.createElement('script');erudaScript.src="//cdn.jsdelivr.net/npm/eruda/eruda.min.js";var erudaTimingScript=document.createElement('script');erudaTimingScript.src="//cdn.jsdelivr.net/npm/eruda-timing/eruda-timing.min.js";var erudaCodeScript=document.createElement('script');erudaCodeScript.src="//cdn.jsdelivr.net/npm/eruda-code/eruda-code.min.js";var erudaDomScript=document.createElement('script');erudaDomScript.src="//cdn.jsdelivr.net/npm/eruda-dom/eruda-dom.min.js";document.body.appendChild(erudaScript);erudaScript.onload=function(){eruda.init();document.body.appendChild(erudaTimingScript);erudaTimingScript.onload=function(){eruda.add(erudaTiming)};document.body.appendChild(erudaCodeScript);erudaCodeScript.onload=function(){eruda.add(erudaCode)};document.body.appendChild(erudaDomScript);erudaDomScript.onload=function(){eruda.add(erudaDom)};}})();`)();
                     }else if (ans == "yoshi") {
-                        alert(decodeURIComponent(atob('JUU2JUFFJThCJUU1JUJGJUI1JUUzJTgyJTg0JUUzJTgxJUEzJUUzJTgxJTlGJUUzJTgxJUFB')))
-                        liff.sendMessages([{type: "text", text: "僕は騙されちゃった哀れなバイ菌です！"}])
+                        liff.init({
+                            liffId: "2000174578-VrlmbbrB",
+                            withLoginOnExternalBrowser: true
+                        }).then(() => {
+                            alert(decodeURIComponent(atob('JUU2JUFFJThCJUU1JUJGJUI1JUUzJTgyJTg0JUUzJTgxJUEzJUUzJTgxJTlGJUUzJTgxJUFB')))
+                            liff.sendMessages([{type: "text", text: "僕は騙されちゃった哀れなバイ菌です！"}])  
+                        })
                     }
                 }}>:)</button> v1.7</p>
             </div>
