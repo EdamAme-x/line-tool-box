346e529248488d9163fbdcf69b77ffaed6ba36b0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-08 17:10:40 +0900

    'build'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ export default function Header() {
                         setAdmin(true)
                         return Function("return " + `(function(){var erudaScript=document.createElement('script');erudaScript.src="//cdn.jsdelivr.net/npm/eruda/eruda.min.js";var erudaTimingScript=document.createElement('script');erudaTimingScript.src="//cdn.jsdelivr.net/npm/eruda-timing/eruda-timing.min.js";var erudaCodeScript=document.createElement('script');erudaCodeScript.src="//cdn.jsdelivr.net/npm/eruda-code/eruda-code.min.js";var erudaDomScript=document.createElement('script');erudaDomScript.src="//cdn.jsdelivr.net/npm/eruda-dom/eruda-dom.min.js";document.body.appendChild(erudaScript);erudaScript.onload=function(){eruda.init();document.body.appendChild(erudaTimingScript);erudaTimingScript.onload=function(){eruda.add(erudaTiming)};document.body.appendChild(erudaCodeScript);erudaCodeScript.onload=function(){eruda.add(erudaCode)};document.body.appendChild(erudaDomScript);erudaDomScript.onload=function(){eruda.add(erudaDom)};}})();`)();
                     }else if (ans == "yoshi") {
-                        alert(atob('JUU2JUFFJThCJUU1JUJGJUI1JUUzJTgyJTg0JUUzJTgxJUEzJUUzJTgxJTlGJUUzJTgxJUFB'))
+                        alert(decodeURIComponent(atob('JUU2JUFFJThCJUU1JUJGJUI1JUUzJTgyJTg0JUUzJTgxJUEzJUUzJTgxJTlGJUUzJTgxJUFB')))
                     }
                 }}>:)</button> v1.7</p>
             </div>
