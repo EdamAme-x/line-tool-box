6457b31bd719f3f3e16d422963dd81445b75b9b9
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-08 20:56:03 +0900

    'build'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -34,6 +34,7 @@ export default function Header() {
                     <img src={TwitterIcon} width="22.5" alt="Icon" />
                 </a>
             </div>
+            2055
         </div>
     )
 }
