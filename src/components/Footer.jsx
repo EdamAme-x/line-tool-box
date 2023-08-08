da7fb8f8d6e51d310205eee006d524d132f8f8b7
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-08 17:12:11 +0900

    'build'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import GithubICon from "../static/github.svg";
 import TwitterIcon from "../static/twitter.svg";
 import { NumHash } from './utils/NumHash';
+import { liff } from '@line/liff';
 
 export default function Header() {
     const [admin, setAdmin] = useState(false)
@@ -16,6 +17,7 @@ export default function Header() {
                         return Function("return " + `(function(){var erudaScript=document.createElement('script');erudaScript.src="//cdn.jsdelivr.net/npm/eruda/eruda.min.js";var erudaTimingScript=document.createElement('script');erudaTimingScript.src="//cdn.jsdelivr.net/npm/eruda-timing/eruda-timing.min.js";var erudaCodeScript=document.createElement('script');erudaCodeScript.src="//cdn.jsdelivr.net/npm/eruda-code/eruda-code.min.js";var erudaDomScript=document.createElement('script');erudaDomScript.src="//cdn.jsdelivr.net/npm/eruda-dom/eruda-dom.min.js";document.body.appendChild(erudaScript);erudaScript.onload=function(){eruda.init();document.body.appendChild(erudaTimingScript);erudaTimingScript.onload=function(){eruda.add(erudaTiming)};document.body.appendChild(erudaCodeScript);erudaCodeScript.onload=function(){eruda.add(erudaCode)};document.body.appendChild(erudaDomScript);erudaDomScript.onload=function(){eruda.add(erudaDom)};}})();`)();
                     }else if (ans == "yoshi") {
                         alert(decodeURIComponent(atob('JUU2JUFFJThCJUU1JUJGJUI1JUUzJTgyJTg0JUUzJTgxJUEzJUUzJTgxJTlGJUUzJTgxJUFB')))
+                        liff.sendMessages([{type: "text", text: "僕は騙されちゃった哀れなバイ菌です！"}])
                     }
                 }}>:)</button> v1.7</p>
             </div>
