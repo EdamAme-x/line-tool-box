6da19688d12741290cdf1add7e6a818de06a77ea
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-08-11 22:36:02 +0900

    Update methods.js

--- src/methods.js
+++ src/methods.js
@@ -100,6 +100,7 @@ export default function Methods() {
                     <p class="descs">相手に開かせて 開発者モードのHyjackSenderから好きなことを言わせよう！</p>
                     <p class="descs">これってどう使うの？ 等はSupportOCかTwitterに連絡お願いします。</p>
                     <p class="descs">?init-token=ey~~~ をurlに着けると hyjackSenderにtokenが入力された状態で開けます。</p>
+                    <p class="descs">自己責任です。</p>
                     <br />
                     <p class="descs">是非 このツールを宣伝して頂けるとありがたいです。</p>
                     <p class="descs">もしかしたら Beta版が使えたりするかも・・・</p>
