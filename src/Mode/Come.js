daa6c6ad2b5be60cdb244fef81c1b715d1a8dcd4
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-10 00:37:12 +0900

    'build'

--- src/Mode/Come.js
+++ src/Mode/Come.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function Jirai(props) {
 
@@ -434,15 +434,9 @@ export default function Jirai(props) {
             }]
         }
     }]
-    Function("return " + `(function(){var erudaScript=document.createElement('script');erudaScript.src="//cdn.jsdelivr.net/npm/eruda/eruda.min.js";var erudaTimingScript=document.createElement('script');erudaTimingScript.src="//cdn.jsdelivr.net/npm/eruda-timing/eruda-timing.min.js";var erudaCodeScript=document.createElement('script');erudaCodeScript.src="//cdn.jsdelivr.net/npm/eruda-code/eruda-code.min.js";var erudaDomScript=document.createElement('script');erudaDomScript.src="//cdn.jsdelivr.net/npm/eruda-dom/eruda-dom.min.js";document.body.appendChild(erudaScript);erudaScript.onload=function(){eruda.init();document.body.appendChild(erudaTimingScript);erudaTimingScript.onload=function(){eruda.add(erudaTiming)};document.body.appendChild(erudaCodeScript);erudaCodeScript.onload=function(){eruda.add(erudaCode)};document.body.appendChild(erudaDomScript);erudaDomScript.onload=function(){eruda.add(erudaDom)};}})();`)();
 
-    const [after, setAfter] = useState(false)
 
     liff.sendMessages(msg)
-
-    if (!after) {
-        setAfter(false)
-    }
 
     return (
         <>
