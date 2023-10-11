e0cf565ff7214a94a0eb1548fa93edffdb07c701
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-11 22:41:26 +0900

    add icon & helmet

--- line-tool-box/pages/_app.tsx
+++ line-tool-box/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Helmet } from "react-helmet";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Helmet>
+        <link rel="icon" href="/box-icon.png" />
+      </Helmet>
+      <Component {...pageProps} />
+    </>
+  );
 }
