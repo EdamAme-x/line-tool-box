787fc7aa8374d4da916c5713f0618526b01819ce
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-12 18:57:14 +0900

    add

--- line-tool-box/src/Layout.tsx
+++ line-tool-box/src/Layout.tsx
@@ -3,14 +3,24 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import css from "@/styles/layout.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Layout(props: { children: React.ReactNode }) {
+
+  const githubIcon = "/svg/github.svg";
+  const twitterIcon = "/svg/twitter.svg";
+
   return (
     <div className={css.layout}>
       <header>
         <Image src="/box-icon.png" alt="icon" width={50} height={50} priority />
-        <h1>LINE-Tool-Box</h1>
+        <h1>LINE-ToolBox</h1>
+        <div className="flex flex-col">
+          <a href="https://github.com/EdamAme-x/line-tool-box?openExternalBrowser=1">
+            <Image src={githubIcon} width={20} height={20} className="m-[7.5px] mx-[3px]" alt="Icon" />
+          </a>
+          <a href="https://twitter.com/amex2189?openExternalBrowser=1">
+            <Image src={twitterIcon} width={20} height={20} className="m-[7.5px] mx-[3px]" alt="Icon" />
+          </a>
+        </div>
       </header>
       <div>{props.children}</div>
     </div>
--- line-tool-box/src/components/Info.tsx
+++ line-tool-box/src/components/Info.tsx
@@ -5,11 +5,16 @@ import { useState } from "react";
 export function Info({ liffId }: Props) {
   return (
     <>
-      <div className="flex flex-wrap justify-around w-full p-10"  suppressHydrationWarning>
+      <div
+        className="flex flex-wrap justify-around w-full p-10"
+        suppressHydrationWarning
+      >
         <Tooltip>
           <Information liffId={liffId} />
         </Tooltip>
-        <Tooltip>info</Tooltip>
+        <Tooltip>
+          <Notification liffId={liffId} />
+        </Tooltip>
       </div>
     </>
   );
@@ -30,10 +35,76 @@ function Information({ liffId }: Props): JSX.Element {
     .catch((_e) => {});
 
   return (
-    <div className="font-mono font-bold">
+    <div className="font-mono font-bold p-3">
       Status: {setup ? "Success" : "Failed"} <br />
-      Platform: {liff.isInClient() ? "LIFF / " + liff.getOS() : "Browser / " + liff.getOS()}<br />
-      Version: {liff.getVersion()}<br />
+      Platform:{" "}
+      {liff.isInClient()
+        ? "LIFF / " + liff.getOS()
+        : "Browser / " + liff.getOS()}
+      <br />
+      Version: v{liff.getVersion()}
+      <br />
     </div>
+  );
+}
+
+function Notification({ liffId }: Props): JSX.Element {
+  return (
+    <div className="font-mono font-bold p-1 flex flex-col items-center">
+      <div className="flex">
+        <NotifyIcon className="w-5 h-5 ml-3 mr-2" />
+        <h2>お知らせ & ヘルプ</h2>
+      </div>
+      <div className="flex flex-col w-[90%] h-[120px] overflow-y-scroll overflow-x-hidden p-2 rounded-lg shadow-2xl">
+        <NotifyMessage title="お知らせ" description="お知らせ" />
+        <NotifyMessage title="ヘルプ" description="ヘルプ" />
+        <NotifyMessage title="お知らせ" description="お知らせ" />
+        <NotifyMessage title="ヘルプ" description="ヘルプ" />
+      </div>
+    </div>
+  );
+}
+
+function NotifyMessage({ title, description }: Props) {
+
+  const popup = () => {
+    
+  }
+    
+  return (
+    <>
+      <div className="border-b border-gray-900 m-1">{title}</div>
+    </>
+  );
+}
+
+function NotifyIcon({ className }: Props) {
+  return (
+    <svg
+      className={className}
+      fill="#000000"
+      height="200px"
+      width="200px"
+      version="1.2"
+      baseProfile="tiny"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 448 512"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <g>
+          {" "}
+          <path d="M222.987,510c31.418,0,57.529-22.646,62.949-52.5H160.038C165.458,487.354,191.569,510,222.987,510z"></path>{" "}
+          <path d="M432.871,352.059c-22.25-22.25-49.884-32.941-49.884-141.059c0-79.394-57.831-145.269-133.663-157.83h-4.141 c4.833-5.322,7.779-12.389,7.779-20.145c0-16.555-13.42-29.975-29.975-29.975s-29.975,13.42-29.975,29.975 c0,7.755,2.946,14.823,7.779,20.145h-4.141C120.818,65.731,62.987,131.606,62.987,211c0,108.118-27.643,118.809-49.893,141.059 C-17.055,382.208,4.312,434,47.035,434H398.93C441.568,434,463.081,382.269,432.871,352.059z"></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
   );
 }
