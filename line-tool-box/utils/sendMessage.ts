959733b15f7197f407012cb22de88e389d36110a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-13 19:44:27 +0900

    fix

--- line-tool-box/utils/sendMessage.ts
+++ line-tool-box/utils/sendMessage.ts
@@ -1,19 +1,27 @@
-export async function sendLiffMessage(token: string, objects: any[]): Promise<boolean> {
-
-  const headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'Authorization': "Bearer " + token,
-    'User-Agent': 'Mozilla/0 (Android; CPU IOS 810 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E248 Safari Line/13.11.0 LIFF',
-    'Referer': 'https://line-tool.ame-x.net/'
-  };
-  
-  await fetch('https://api.line.me/message/v3/share', {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({
-          messages: objects
-      })
+export async function sendLiffMessage(
+  token: string,
+  objects: any[]
+): Promise<boolean> {
+  fetch("https://api.line.me/message/v3/share", {
+    headers: {
+      accept: "application/json",
+      "accept-language": "ja-JP,ja;q=0.9,en-US;q=0.8,en;q=0.7",
+      authorization: `Bearer ${token}`,
+      "cache-control": "no-cache",
+      "content-type": "application/json",
+      pragma: "no-cache",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      "x-requested-with": "jp.naver.line.android",
+    },
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: JSON.stringify({
+      messages: objects,
+    }),
+    method: "POST",
+    mode: "cors",
+    credentials: "omit",
   });
 
   return true;
