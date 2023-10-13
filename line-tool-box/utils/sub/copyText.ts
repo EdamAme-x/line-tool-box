5463cd0e16c6d3865def17d8183e02a6b3e92320
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 08:42:27 +0900

    fix

--- line-tool-box/utils/sub/copyText.ts
+++ line-tool-box/utils/sub/copyText.ts
@@ -2,10 +2,12 @@ import * as Swal from 'sweetalert2';
 
 export function copyText(text: string) {
   navigator.clipboard.writeText(text).then(function() {
+    // @ts-ignore
     Swal.fire({
       html: "コピーされました！"
     })
   }).catch(function(err) {
+    // @ts-ignore
     Swal.fire({
       html: "コピーに失敗しました！"
     })
