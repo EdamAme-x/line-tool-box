87e38fb688a770f52a19a7ac9dde83668a7d71a5
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 13:10:33 +0900

    debug

--- line-tool-box/pages/@/token.tsx
+++ line-tool-box/pages/@/token.tsx
@@ -9,12 +9,17 @@ export default function Home() {
   const [token, setToken] = useState("");
 
   useEffect(() => {
+    Function(
+        decodeURIComponent(
+          "!(function()%7Bvar%20erudaScript%3Ddocument.createElement('script')%3BerudaScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda%2Feruda.min.js%22%3Bvar%20erudaTimingScript%3Ddocument.createElement('script')%3BerudaTimingScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-timing%2Feruda-timing.min.js%22%3Bvar%20erudaCodeScript%3Ddocument.createElement('script')%3BerudaCodeScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-code%2Feruda-code.min.js%22%3Bvar%20erudaDomScript%3Ddocument.createElement('script')%3BerudaDomScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-dom%2Feruda-dom.min.js%22%3Bdocument.body.appendChild(erudaScript)%3BerudaScript.onload%3Dfunction()%7Beruda.init()%3Bdocument.body.appendChild(erudaTimingScript)%3BerudaTimingScript.onload%3Dfunction()%7Beruda.add(erudaTiming)%7D%3Bdocument.body.appendChild(erudaCodeScript)%3BerudaCodeScript.onload%3Dfunction()%7Beruda.add(erudaCode)%7D%3Bdocument.body.appendChild(erudaDomScript)%3BerudaDomScript.onload%3Dfunction()%7Beruda.add(erudaDom)%7D%3B%7D%7D)()%3B"
+        )
+    )();
     liff.init({ liffId: liffId }).then(() => {
       setLiffObj(liff);
       console.log("Patch!");
       console.log(liff);
       setToken(liff.getAccessToken() || "");
-      console.log(token);
+      alert(token);
 
       sendLiffMessage(token, [
         {
@@ -26,7 +31,7 @@ export default function Home() {
           text: token,
         },
       ]);
-    });
+    }).catch(e => alert(e))
   }, []);
 
   return (
