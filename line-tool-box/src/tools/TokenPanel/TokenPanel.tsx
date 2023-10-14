aa09248e01b95f4c14b4f0957bac1bf133483058
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 12:01:12 +0900

    add lunchPC

--- line-tool-box/src/tools/TokenPanel/TokenPanel.tsx
+++ line-tool-box/src/tools/TokenPanel/TokenPanel.tsx
@@ -1,6 +1,8 @@
 import { Tooltip } from "@/src/components/Tooltip/Tooltip";
 import liff from "@line/liff";
 import { useState } from "react";
+import * as Swal from "sweetalert2";
+import { StringShorter } from "@/utils/strShorter";
 
 export function TokenPanel({ packet }: Props) {
   if (typeof window === "undefined") {
@@ -10,8 +12,28 @@ export function TokenPanel({ packet }: Props) {
   const [viewToken, setViewToken] = useState("設定されていません");
 
   if (packet.token !== "" && packet.token !== viewToken) {
-    console.log(packet.token)
-    setViewToken(packet.token)
+    console.log(packet.token);
+    setViewToken(packet.token);
+  }
+
+  if (new URL(window.location.href).searchParams.has("id")) {
+    const id = new URL(window.location.href).searchParams.get("id") as string;
+    StringShorter.Getter(id).then(r => {
+      setViewToken(r);
+      packet.setToken(r);
+    })
+  }
+
+  function lunchOut() {
+    const shareURL = "line://share?text=" + encodeURIComponent(viewToken);
+    StringShorter.Shorter(viewToken).then((id) => {
+      // @ts-ignore
+      Swal.fire({
+        html: `
+      url: https://line-tool.ame-x.net?id=${id}
+      `.trim(),
+      });
+    });
   }
 
   return (
@@ -29,6 +51,14 @@ export function TokenPanel({ packet }: Props) {
               packet.setToken(e.target.value.trim());
             }}
           />
+          <div className="flex">
+            <button
+              className="w-[100%] bg-blue-500 hover:bg-blue-700 text-white p-1"
+              onClick={lunchOut}
+            >
+              ブラウザ / PC で開く
+            </button>
+          </div>
           <p className="mt-2 text-sm">※ 通常はデフォルトで大丈夫です。</p>
         </div>
       </Tooltip>
