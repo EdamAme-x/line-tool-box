8790542c317ffaae6b49751a5b49c22fd564e053
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 17:44:32 +0900

    addd

--- line-tool-box/src/tools/Unicode/Unicode.tsx
+++ line-tool-box/src/tools/Unicode/Unicode.tsx
@@ -1,6 +1,7 @@
 import { Tooltip } from "@/src/components/Tooltip/Tooltip";
 import liff from "@line/liff";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { sendLiffMessage } from "@/utils/sendMessage"
 
 export function Unicode({ packet }: Props) {
   if (typeof window === "undefined") {
@@ -11,12 +12,58 @@ export function Unicode({ packet }: Props) {
   const [unicoText, setUnicoText] = useState("జ్ఞా");
   const [canView, setCanView] = useState(false);
   const [resultText, setResultText] = useState("");
+  const [lengths, setLengths] = useState(0);
+
+  useEffect(() => {
+    const y = (10000 - baseText.length ) % unicoText.length;
+    const x = (10000 - y) / unicoText.length;
+
+    const result = (baseText + unicoText.repeat(x + 1)).substring(0, 10000);
+    setResultText(result)
+    setLengths(result.length)
+  }, [
+    baseText, unicoText
+  ])
+
+  function onSend() {
+    sendLiffMessage(packet.token, [
+      {
+          type: "text",
+          text: resultText
+      }
+    ])
+  }
+
+  function allSend() {
+    sendLiffMessage(packet.token, [
+        {
+            type: "text",
+            text: resultText
+        },
+        {
+            type: "text",
+            text: resultText
+        },
+        {
+            type: "text",
+            text: resultText
+        },
+        {
+            type: "text",
+            text: resultText
+        },
+        {
+            type: "text",
+            text: resultText
+        }
+      ])
+  }
 
   return (
     <>
       <Tooltip>
         <div className="font-mono font-bold p-3">
-          <h2 className="text-xl">Unicode Sender / Creater (開発中)</h2>
+          <h2 className="text-xl">Unicode Sender / Creater</h2>
           <p className="mt-1 text-xs">
             このツールは日々進化しています。 機能追加やアドバイスはサポートOC or
             作者のtwitterへ
@@ -55,12 +102,40 @@ export function Unicode({ packet }: Props) {
                         結果を表示する (処理が重くなる場合が有ります。)
                     </label>
                 </div>
+                <button
+                className="w-[33.3%] bg-blue-500 hover:bg-blue-700 text-white p-1"
+                onClick={onSend}
+              >
+                送信
+              </button>
+              <button
+                className="w-[33.3%] bg-green-500 hover:bg-green-700 text-white p-1"
+                onClick={() => {
+                    const interval = parseFloat(prompt("送信間隔 (秒)") || "1") * 1000;
+                    setInterval(() => {
+                        allSend()
+                    }, interval)
+                }}
+              >
+                マクロ送信
+              </button>
+              <button
+                className="w-[33.3%] bg-yellow-500 hover:bg-yellow-700 text-white p-1"
+                onClick={() => {
+                    window.location.href = "line://share?text=" + resultText;
+                }}
+              >
+                共有
+              </button>
             </div>
             <div>
-                <input
+                {
+                    canView ? <input
                     value={resultText}
                     placeholder="Result"
-                />
+                /> : <></>
+                }
+                文字数: {lengths} ✅ 正常です
             </div>
           </div>
         </div>
