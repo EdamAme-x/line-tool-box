743ab908fc9628ac91bbc020a8e77f31184c7ecc
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 18:22:25 +0900

    as

--- line-tool-box/src/tools/Unicode/Unicode.tsx
+++ line-tool-box/src/tools/Unicode/Unicode.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { sendLiffMessage } from "@/utils/sendMessage";
 
 
-function shuffleArray(array) {
+function shuffleArray(array: string[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
