2ab0d9ee99b5876e78d710c30d075588e2dea060
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-14 16:50:44 +0900

    add

--- line-tool-box/src/tools/Unicode/Unicode.tsx
+++ line-tool-box/src/tools/Unicode/Unicode.tsx
@@ -7,12 +7,41 @@ export function Unicode({ packet }: Props) {
     return <div></div>;
   }
 
+  const [baseText, setBaseText] = useState("unico");
+  const [unicoText, setUnicoText] = useState("జ్ఞా");
+
   return (
     <>
       <Tooltip>
         <div className="font-mono font-bold p-3">
-          <h2 className="text-xl">Unicode Sender / Creater</h2>
-          <p>Coming soon..</p>
+          <h2 className="text-xl">Unicode Sender / Creater (開発中)</h2>
+          <p className="mt-1 text-xs">
+            このツールは日々進化しています。 機能追加やアドバイスはサポートOC or
+            作者のtwitterへ
+          </p>
+          <div className="mt-2 flex flex-col items-center">
+            <div className="flex">
+              <span className="text-xl">Base :</span>{" "}
+              <input
+                value={baseText}
+                onChange={(e) => setBaseText(e.target.value)}
+                placeholder="最初に付く文字"
+              />
+            </div>
+            <br />
+            <div>
+              後に付けるUnicode:{" "}
+              <input
+                value={unicoText}
+                onChange={(e) => setUnicoText(e.target.value)}
+                placeholder="Unicode"
+              />
+              <br />
+              <button className="w-[100%] bg-blue-500 hover:bg-blue-700 text-white p-1 text-xs mt-1">
+                Unicodeの文字一覧とテンプレートを見る
+              </button>
+            </div>
+          </div>
         </div>
       </Tooltip>
     </>
