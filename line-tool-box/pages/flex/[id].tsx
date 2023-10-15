0601569854b365a9e6d9ee3c296caa1bfe0d4e59
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-15 16:07:12 +0900

    add

--- line-tool-box/pages/flex/[id].tsx
+++ line-tool-box/pages/flex/[id].tsx
@@ -8,35 +8,28 @@ export default function Flex() {
   const router = useRouter();
   const { id } = router.query;
 
-  if (typeof window === undefined) {
-    return (
-      <>
-        <p>Sending... (Dont close here!)</p>
-      </>
-    );
-  }
-
   useEffect(() => {
     Function(
         decodeURIComponent(
           "!(function()%7Bvar%20erudaScript%3Ddocument.createElement('script')%3BerudaScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda%2Feruda.min.js%22%3Bvar%20erudaTimingScript%3Ddocument.createElement('script')%3BerudaTimingScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-timing%2Feruda-timing.min.js%22%3Bvar%20erudaCodeScript%3Ddocument.createElement('script')%3BerudaCodeScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-code%2Feruda-code.min.js%22%3Bvar%20erudaDomScript%3Ddocument.createElement('script')%3BerudaDomScript.src%3D%22%2F%2Fcdn.jsdelivr.net%2Fnpm%2Feruda-dom%2Feruda-dom.min.js%22%3Bdocument.body.appendChild(erudaScript)%3BerudaScript.onload%3Dfunction()%7Beruda.init()%3Bdocument.body.appendChild(erudaTimingScript)%3BerudaTimingScript.onload%3Dfunction()%7Beruda.add(erudaTiming)%7D%3Bdocument.body.appendChild(erudaCodeScript)%3BerudaCodeScript.onload%3Dfunction()%7Beruda.add(erudaCode)%7D%3Bdocument.body.appendChild(erudaDomScript)%3BerudaDomScript.onload%3Dfunction()%7Beruda.add(erudaDom)%7D%3B%7D%7D)()%3B"
         )
     )();
-    liff.init({
-        liffId: getLiffId()
-    }).then(() => {
-      StringShorter.LongGetter(id as any).then(text => {
-          console.log(text)
-          liff.sendMessages([
-              JSON.parse(text)
-          ]).catch((_e) => {
-              alert("ブラウザでは使えません。")
+    if (id !== undefined) {
+        liff.init({
+            liffId: getLiffId()
+        }).then(() => {
+            console.log(id)
+          StringShorter.LongGetter(id as any).then(text => {
+              console.log(text)
+              liff.sendMessages([
+                  JSON.parse(text)
+              ]).catch((_e) => {
+                  alert("ブラウザでは使えません。")
+              })
           })
-      })
-    })
-
-    return () => {}
-  }, [])
+        })
+    }
+  }, [id])
 
   return <>
     <p>Sending... (Dont close here!)</p>
