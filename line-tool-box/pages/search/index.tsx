7830c58c6085c171c83a05f97c0b57382a692649
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-17 15:02:11 +0900

    fix

--- line-tool-box/pages/search/index.tsx
+++ line-tool-box/pages/search/index.tsx
@@ -1,21 +1,32 @@
-
-import SearchContent from './../../src/search/content';
-import { Helmet } from 'react-helmet';
+import { useState } from "react";
+import SearchContent from "./../../src/search/content";
+import { Helmet } from "react-helmet";
 
 export default function Search() {
-    return <>
-        <Helmet>
-            <title>OpenChat PowerSearch</title>
-        </Helmet>
-        <div className="font-mono font-bold w-[100vw] h-[100vh] bg-gray-800 flex justify-center items-center" style={{
-            backgroundImage: "url('/graphic.png')"
-        }}>
-            <div className="min-w-[350px] min-h-[100vh] max-w-[350px] max-h-[100vh] flex flex-col justify-around items-center">
-                <SearchContent />
-            </div>
-            <div className='fixed bottom-0 text-white'>
-                Created by @amex2189 &lt;= 是非フォローしてね
-            </div>
+  const [view, setView] = useState(true);
+
+  return (
+    <>
+      <Helmet>
+        <title>OpenChat PowerSearch</title>
+      </Helmet>
+      <div
+        className="font-mono font-bold w-[100vw] h-[100vh] bg-gray-800 flex justify-center items-center"
+        style={{
+          backgroundImage: "url('/graphic.png')",
+        }}
+      >
+        <div onClick={() => setView(false)} className="min-w-[350px] min-h-[100vh] max-w-[350px] max-h-[100vh] flex flex-col justify-around items-center">
+          <SearchContent />
         </div>
-    </>;
+        {view ? (
+          <div className="fixed bottom-0 text-white">
+            Created by @amex2189 &lt;= 是非フォローしてね
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </>
+  );
 }
