2f1864468e55813e646a0dde6fa85c409e82a43e
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-10-25 18:54:09 +0900

    Update notifys.jsx

--- line-tool-box/src/components/datas/notifys.jsx
+++ line-tool-box/src/components/datas/notifys.jsx
@@ -1,18 +1,18 @@
 export const Notifys = [
-    {
-        title: "新ツールを作成しました。",
-        description: `
-        <div style="overflow-y: scroll; height: 50vh;">
-            <p>どうも このツールの作者の ame_x です。</p>
-            <p>このツールは昔作った <a href="https://line.naver.jp/R/app/2000174578-VrlmbbrB">https://line.naver.jp/R/app/2000174578-VrlmbbrB</a>の後継です。</p>
-            <p>ここの欄にはツールのQ & Aや仕様情報・アップデート情報等を書いていきます。 このツールはどうやって使うの？と言ったものは</p>
-            <p>私のTwitter @amex2189 、若しくはSupportOCの https://honmono.ame-x.net まで。</p>
-            <p>最後に是非私の Twitter をフォローしてください。 モチベに繋がります。 寄付等もどうぞ 色々優遇します、</p>
-            <p>ちなみに当然ですが自己責任です。 ただし悪用するなとは言いませんし、しろとも言いません。</p>
-            <p>まぁそういう事です。</p>
-        </div>
-        `
-    },
+    // {
+    //     title: "新ツールを作成しました。",
+    //     description: `
+    //     <div style="overflow-y: scroll; height: 50vh;">
+    //         <p>どうも このツールの作者の ame_x です。</p>
+    //         <p>このツールは昔作った <a href="https://line.naver.jp/R/app/2000174578-VrlmbbrB">https://line.naver.jp/R/app/2000174578-VrlmbbrB</a>の後継です。</p>
+    //         <p>ここの欄にはツールのQ & Aや仕様情報・アップデート情報等を書いていきます。 このツールはどうやって使うの？と言ったものは</p>
+    //         <p>私のTwitter @amex2189 、若しくはSupportOCの https://honmono.ame-x.net まで。</p>
+    //         <p>最後に是非私の Twitter をフォローしてください。 モチベに繋がります。 寄付等もどうぞ 色々優遇します、</p>
+    //         <p>ちなみに当然ですが自己責任です。 ただし悪用するなとは言いませんし、しろとも言いません。</p>
+    //         <p>まぁそういう事です。</p>
+    //     </div>
+    //     `
+    // },
     {
         title: "リンク集",
         description: `
@@ -77,10 +77,6 @@ emidとか通報リンクが抜ける自作オープンチャット検索サイ�
 https://zenn.dev/ame_x/articles/6fab5d74dcced7
 ツールについての質問や追加要望、バグ報告はサポートOC (https://honmono.ame-x.net)　までお願いします。
         `
-    },
-    {
-        title: "ツールが規制されたので新しいurl用意中",
-        description: "運営に直接規制されたので新しいurl準備中です"
     },
     {
         title: "1部の機能が使えなくなります。",
